3daf04235aa4760a3365a7d721d4f7ad95a61fd4
Elie Bou Nehme <eliebn147@outlook.com>
2022-05-11 00:57:06 +0300

    Implemented feedback route to get all feedbacks

--- routes/feedback-routes.js
+++ routes/feedback-routes.js
@@ -1,40 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const FeedbackModel = require('../models/FeedbackModel.js');
+const FeedbackModel = require("../models/FeedbackModel.js");
 
-router.post('/create',                                       
-    function(req, res) {
+router.post("/create", function (req, res) {
+  const document = {
+    email: req.body.email,
+    message: req.body.message,
+    avatar: req.body.avatar,
+  };
 
-        const document = {
-            "email": req.body.email,
-            "message" : req.body.message
-        };
+  FeedbackModel.create(document)
+    .then(function (dbDocument) {
+      res.json({
+        document: dbDocument,
+        message: "Feedback created",
+      });
+    })
+    .catch(function (dbError) {
+      console.log("DB feedback create error", dbError);
+      res.json({
+        message: "feedback create error",
+      });
+    });
+});
 
-        FeedbackModel
-        .create(document)
-        .then(
-            function(dbDocument) {
-                res.json(
-                    {
-                        document: dbDocument,
-                        message: "Feedback created"
-                    }
-                );
-            }
-        )
-        .catch(
-            function(dbError) {
-                console.log('DB feedback create error', dbError);
-                res.json(
-                    {
-                        message: "feedback create error"
-                    }
-                );
-            }
-        );
-    }
-);
-
+router.get("all", function (req, res) {
+  FeedbackModel
+  .find()
+  .then(function (dbDocument) {
+    res.json({
+      message: "All the feedback",
+      document: dbDocument,
+    });
+  })
+  .catch(function(error){
+    res.json({
+        message : "Error in feedback/all",
+        error : error
+    })
+})
+  
+});
 
 module.exports = router;
